897a834c604dc7c620958d10f43117b155a275bd
Chris <chrisfjohnson3@gmail.com>
2020-10-01 18:23:32 -0600

    having a problem with this key word not referring to correct div and or not changing the class

--- js-scripts/loadingScripts.js
+++ js-scripts/loadingScripts.js
@@ -15,21 +15,9 @@ function change() {
     index++;
     setTimeout(change, 20000 )
 }
+/* probably dont need the above method */
 
 
-/* loading animation */
-/* var time;
-function loading() {
-    time = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("index-body").style.display = "block";    
-    console.log("in loading"); /**************** for testing ***************
-
-}
- */
 
 function load() {
     setTimeout(function() {
@@ -39,4 +27,10 @@ function load() {
 
 function menuButton(x){
     x.classList.toggle("change");
+}
+
+function expand1() {
+    y = document.getElementsByClassName("webpage-screen");
+    y.classList.toggle("expanded1")
+
 }
